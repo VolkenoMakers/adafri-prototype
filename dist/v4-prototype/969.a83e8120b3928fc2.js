d61318f77ac3b09988faf0291222e0ac8454d754
Sadiosan <knfree99@gmail.com>
2023-06-07 14:15:38 +0000

    clear git and github actions settings

---
+++ dist/v4-prototype/969.a83e8120b3928fc2.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkv4_prototype=self.webpackChunkv4_prototype||[]).push([[969],{969:(_,t,u)=>{u.r(t),u.d(t,{SecurityModule:()=>l});var r=u(6895),c=u(294),e=u(4650);let l=(()=>{class o{}return o.\u0275fac=function(M){return new(M||o)},o.\u0275mod=e.oAB({type:o}),o.\u0275inj=e.cJS({imports:[r.ez,c.I]}),o})()}}]);
